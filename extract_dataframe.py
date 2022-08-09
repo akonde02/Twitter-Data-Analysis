ec06f94c199c9efe45412d2535b031b071baa020
Prince Akonde <akon.de.prince2g5@gmail.com>
2022-08-09 18:48:00 +0000

    fixed find retweet count

--- extract_dataframe.py
+++ extract_dataframe.py
@@ -101,7 +101,13 @@ class TweetDfExtractor:
         return favorite_count
     
     def find_retweet_count(self)->list:
-        retweet_count = 
+        retweet_count = []
+        for i in self.tweets_list:
+            if 'retweeted_status' in i.keys():
+                retweet_count.append(i['retweeted_status']['retweet_count'])
+            else:
+                retweet_count.append(i['retweet_count'])
+        return retweet_count
 
     def find_hashtags(self)->list:
         hashtags =
